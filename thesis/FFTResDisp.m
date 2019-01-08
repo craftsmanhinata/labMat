a0826cc10565ac2713dbbc63240f4d78f81bad2d
kijitoragouf <173426020@ccalumni.meijo-u.ac.jp>
2019-01-08 16:34:59 +0900

    autoTable generate program added.

--- thesis/FFTResDisp.m
+++ thesis/FFTResDisp.m
@@ -1,9 +1,14 @@
 %変数をロードしてから…
 clc;
 close all;
+paramLength = 2;
 evalAxis = [TriAccKey TriGyroKey TriAngleKey];
 NLMSRMSEMean = zeros(trialLength,searchFilterCoefLengthProcNum,FFTLMSStepProcNum,length(evalAxis));
 RMSEResponse = zeros(length(evalAxis),trialLength);
+filterParamResponse = zeros(length(evalAxis),trialLength,paramLength);
+coefLengthParamNum = 1;
+adapParamNum = 2;
+dispDigit = 4;
 disp('適応フィルタアルゴリズムによる評価結果');
 for trialIndex = 1 : trialLength
     otherDataIndex = 1:1:trialLength;
@@ -20,19 +25,22 @@ for trialIndex = 1 : trialLength
         'ステップサイズ:',num2str(FFTStepSizeArray(StepInd)),''));
         disp(strcat(num2str(trialIndex),'個目のデータのRMSE:',num2str(FFTLMSRMSEArray(trialIndex,coefLenInd,StepInd,evalAxis(axisIndex)))));
         RMSEResponse(axisIndex,trialIndex) = FFTLMSRMSEArray(trialIndex,coefLenInd,StepInd,evalAxis(axisIndex));
+        filterParamResponse(axisIndex,trialIndex,coefLengthParamNum) = searchFilterCoefLength(coefLenInd);
+        filterParamResponse(axisIndex,trialIndex,adapParamNum) = FFTStepSizeArray(StepInd);
     end
 end
 
-disp(strcat('加速度センサのRMSEの平均値',num2str(mean(RMSEResponse(1,:)))));
-disp(strcat('加速度センサのRMSEの標準偏差',num2str(std(RMSEResponse(1,:)))));
-disp(strcat('ジャイロセンサのRMSEの平均値',num2str(mean(RMSEResponse(2,:)))));
-disp(strcat('ジャイロセンサのRMSEの標準偏差',num2str(std(RMSEResponse(2,:)))));
-disp(strcat('角速度のRMSEの平均値',num2str(mean(RMSEResponse(3,:)))));
-disp(strcat('角速度のRMSEの標準偏差',num2str(std(RMSEResponse(3,:)))));
+disp(strcat(Dict(evalAxis(1)),'のRMSEの平均値',num2str(mean(RMSEResponse(1,:)))));
+disp(strcat(Dict(evalAxis(1)),'のRMSEの標準偏差',num2str(std(RMSEResponse(1,:)))));
+disp(strcat(Dict(evalAxis(2)),'のRMSEの平均値',num2str(mean(RMSEResponse(2,:)))));
+disp(strcat(Dict(evalAxis(2)),'のRMSEの標準偏差',num2str(std(RMSEResponse(2,:)))));
+disp(strcat(Dict(evalAxis(3)),'のRMSEの平均値',num2str(mean(RMSEResponse(3,:)))));
+disp(strcat(Dict(evalAxis(3)),'のRMSEの標準偏差',num2str(std(RMSEResponse(3,:)))));
 [p,tbl,stats] = anova1(RMSEResponse');
 [c,m,h,nms] = multcompare(stats,'CType','bonferroni');
-disp('加速度と角速度のt検定の結果');
+disp(strcat(Dict(evalAxis(1)),'と',Dict(evalAxis(2)),'のt検定の結果'));
 [h,p] = ttest2(RMSEResponse(1,:),RMSEResponse(2,:),'Vartype','unequal')
+
 
 figure();
 FontSize = 20;
@@ -46,3 +54,38 @@ er.LineStyle = 'none';
 ylabel('RMSE','FontSize',FontSize);
 set(gca,'xticklabel',{'Acceleration','Gyro','Angular Speed'});
 set(gca,'FontSize',FontSize);
+
+%tableの作製
+AdapFiltParamName = '$\mu$';
+fprintf('\\begin{table}[H]\n');
+fprintf('\t\\begin{center}\n');
+fprintf('\t\t\\caption{dummyCaption}\n');
+fprintf('\t\t\\footnotesize \\begin{tabular}{|c|c|c|c|c|c|c|c|c|c|} \\hline\n');
+fprintf('\t\t\t{}&\\multicolumn{3}{c|}{%s}&\\multicolumn{3}{c|}{%s}&\\multicolumn{3}{c|}{%s}\\\\ \\hline\n',Dict(evalAxis(1)),Dict(evalAxis(2)),Dict(evalAxis(3)));
+fprintf('\t\t\t{DataSet}&{$N$}&{%s}&{$RMSE$}&{$N$}&{%s}&{$RMSE$}&{$N$}&{%s}&{$RMSE$}\\\\ \\hline\n',AdapFiltParamName,AdapFiltParamName,AdapFiltParamName);
+
+for trialIndex = 1 : trialLength
+    fprintf('\t\t\t{%d}&',trialIndex);
+    for axisIndex = 1 : length(evalAxis)
+        fprintf('{%d}&{%.*f}&{%.*f}',filterParamResponse(axisIndex,trialIndex,coefLengthParamNum)...
+            ,dispDigit,round(filterParamResponse(axisIndex,trialIndex,adapParamNum),dispDigit)...
+            ,dispDigit,round(RMSEResponse(axisIndex,trialIndex),dispDigit));
+        if axisIndex <= (length(evalAxis) -1)
+            fprintf('&');
+        end
+    end
+    fprintf('\\\\ \\hline \n');
+end
+fprintf('\t\t\t{$MEAN$}&\\multicolumn{2}{c|}{}&{%.*f}&\\multicolumn{2}{c|}{}&{%.*f}&\\multicolumn{2}{c|}{}&{%.*f}\\\\ \\hline\n'...
+    ,dispDigit,round(mean(RMSEResponse(1,:)),dispDigit)...
+    ,dispDigit,round(mean(RMSEResponse(2,:)),dispDigit)...
+    ,dispDigit,round(mean(RMSEResponse(3,:)),dispDigit));
+fprintf('\t\t\t{$SD$}&\\multicolumn{2}{c|}{}&{%.*f}&\\multicolumn{2}{c|}{}&{%.*f}&\\multicolumn{2}{c|}{}&{%.*f}\\\\ \\hline\n'...
+    ,dispDigit,round(std(RMSEResponse(1,:)),dispDigit)...
+    ,dispDigit,round(std(RMSEResponse(2,:)),dispDigit)...
+    ,dispDigit,round(std(RMSEResponse(3,:)),dispDigit));
+fprintf('\t\t\\end{tabular}\n');
+fprintf('\t\t\\label{tab:dummyLabel}\n');
+fprintf('\t\\end{center}\n');
+fprintf('\\end{table}\n');
+
